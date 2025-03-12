6751dd1cd1eff14127edbaf2cddb8e3a596d3b07
SAKTHI PRASANNA J <sakthiprasanna562001@gmail.com>
2025-03-12 10:40:39 +0530

    Transaction

---
+++ Transaction.java
@@ -0,0 +1,31 @@
+package com.qsp.transaction;
+
+import com.qsp.account.Account;
+
+public class Transaction {
+	 public void Deposit(double amt,Account a) {
+			if(amt > 0) {
+				double b = a.getBal()+amt;
+				System.out.println("Deposit Success");
+			}
+			else {
+				System.out.println(" Enter valid amount");
+			}
+		
+		}
+	 
+	 public void Withdraw(double amt,Account a) {
+			if (a.getBal() >= amt) {
+			 double b = a.getBal() - amt;
+			 
+				System.out.println("Withdraw Success");
+			}
+			else {
+				System.out.println("Insuffucient Balance");
+			}
+		}
+	 public void checkBal(Account a) {
+		 System.out.println(a.getBal());
+	 }
+	}
+
